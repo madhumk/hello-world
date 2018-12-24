fa04ba3b33224ad2d2f2d73323bc6efa9231dccc
madhumk <mkmads@yahoo.com>
2018-12-24 11:23:08 +0530

    Add files via upload
    Added initial version

---
+++ Computation.java
@@ -0,0 +1,16 @@
+package com.madhu.alm;
+
+public class Computation {
+	
+	
+	public int addNumbers (int a, int b) {
+		
+		return a + b;
+	}
+	
+	public int multiplyNumbers (int a, int b) {
+		
+		return a*b;
+	}
+
+}
